f1be2f614cb271c2fb7684b2ca20170696ec4ddd
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-26 09:02:08 -0700

    remove confusing log message

--- notify.groovy
+++ notify.groovy
@@ -3,10 +3,9 @@ node {
     dateString = sh([script: 'date --date \'1 week ago\' +%Y-%m-%d', returnStdout: true]).trim()
     sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\""
     
-    stage 'cleanup dead links'
+    stage 'cleanup broken links'
     brokenLinks = sh([script: "find /mnt/data/repository/gbif-idigbio.parquet -xtype l", returnStdout: true]).split('\n')
 
-    echo "found [${brokenLinks.size()}] broken links"
     for (brokenLink in brokenLinks) {
       if (brokenLink.size() > 0) { sh "rm ${brokenLink.trim}" }
     }
