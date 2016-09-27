840d9567718becbff3a47c95e83d3cfd0f2d14be
Jorrit Poelen <jhpoelen@gmail.com>
2016-09-26 17:12:36 -0700

    Update notify.groovy

--- notify.groovy
+++ notify.groovy
@@ -1,12 +1,5 @@
 node {
     stage 'notify subscribers'
     dateString = sh([script: 'date --date \'1 week ago\' +%Y-%m-%d', returnStdout: true]).trim()
-    sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\""
-    
-    stage 'cleanup broken links'
-    brokenLinks = sh([script: "find /mnt/data/repository/gbif-idigbio.parquet -xtype l", returnStdout: true]).split('\n')
-
-    for (brokenLink in brokenLinks) {
-      if (brokenLink.size() > 0) { sh "rm ${brokenLink.trim()}" }
-    }
+    sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\"
 }
