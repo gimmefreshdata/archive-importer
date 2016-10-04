73d56b85162cbb4fac1cec8600f3d14f59a12901
Jorrit Poelen <jhpoelen@gmail.com>
2016-10-04 07:58:59 -0700

    add closing quotes

--- notify.groovy
+++ notify.groovy
@@ -1,5 +1,5 @@
 node {
     stage 'notify subscribers'
     dateString = sh([script: 'date --date \'1 week ago\' +%Y-%m-%d', returnStdout: true]).trim()
-    sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\"
+    sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\""
 }
