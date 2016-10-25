c40ad982cd0acf2a1b4fa2091d29b19d1104f0a1
Jorrit Poelen <jhpoelen@gmail.com>
2016-10-25 12:23:59 -0700

    update job version. related to https://github.com/gimmefreshdata/freshdata/issues/57

--- archives.groovy
+++ archives.groovy
@@ -90,7 +90,7 @@ def requestUpdate() {
   sparkRequest = '''curl -X POST http://@@HOST@@:7077/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
 "action" : "CreateSubmissionRequest",
   "appArgs" : [ "-f", "cassandra","-c","/home/int/data/gbif-idigbio.parquet","-t", "/home/int/data/traitbank/*.csv", "-a", "true" ],
-  "appResource" : "file:///home/int/jobs/iDigBio-LD-assembly-1.5.4.jar",
+  "appResource" : "file:///home/int/jobs/iDigBio-LD-assembly-1.5.5.jar",
   "clientSparkVersion" : "1.6.1",
   "environmentVariables" : {
     "SPARK_ENV_LOADED" : "1"
@@ -125,7 +125,7 @@ def requestConversion() {
   sparkRequest = '''curl -X POST http://@@HOST@@:7077/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
   "action" : "CreateSubmissionRequest",
   "appArgs" : [ "file:///mnt/data/jenkins/workspace/@@JOB_NAME@@/dwca/meta.xml" ],
-  "appResource" : "file:///home/int/jobs/iDigBio-LD-assembly-1.5.3.jar",
+  "appResource" : "file:///home/int/jobs/iDigBio-LD-assembly-1.5.5.jar",
   "clientSparkVersion" : "1.6.1",
   "environmentVariables" : {
     "SPARK_ENV_LOADED" : "1"
