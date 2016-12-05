eaa42f3aeb86e44de7f566e3bff2e7849efcce58
Jorrit Poelen <jhpoelen@gmail.com>
2016-12-05 10:24:26 -0800

    Look back two weeks to pickup changes that were processed by update monitors a week ago.

--- notify.groovy
+++ notify.groovy
@@ -1,5 +1,5 @@
 node {
     stage 'notify subscribers'
-    dateString = sh([script: 'date --date \'1 week ago\' +%Y-%m-%d', returnStdout: true]).trim()
+    dateString = sh([script: 'date --date \'2 weeks ago\' +%Y-%m-%d', returnStdout: true]).trim()
     sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\""
 }
