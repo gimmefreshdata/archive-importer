f4cf54dbf4468fc261b2e04f496133c1bda93e89
Jorrit Poelen <jhpoelen@gmail.com>
2017-05-09 15:01:40 -0700

    enable verbose curl

--- archives.groovy
+++ archives.groovy
@@ -113,6 +113,7 @@ def requestUpdate() {
 }
 
 def submitRequest(request) {
+    echo 'submitting request 
     submissionResponse = sh([script: request, returnStdout: true])
     def submissionIdMatch = submissionResponse =~ 'submissionId"\\s+:\\s+"(.+)"'
     if (!submissionIdMatch) {
@@ -122,7 +123,7 @@ def submitRequest(request) {
 }
 
 def requestConversion() {
-  sparkRequest = '''curl -X POST http://@@HOST@@:7077/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
+  sparkRequest = '''curl --verbose -X POST http://@@HOST@@:7077/v1/submissions/create --header "Content-Type:application/json;charset=UTF-8" --data '{
   "action" : "CreateSubmissionRequest",
   "appArgs" : [ "file:///mnt/data/jenkins/workspace/@@JOB_NAME@@/dwca/meta.xml" ],
   "appResource" : "file:///home/int/jobs/iDigBio-LD-assembly-1.5.5.jar",
