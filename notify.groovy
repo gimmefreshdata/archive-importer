356b2c52f986614243873b680a5353fd1ce06a7c
jhpoelen <jhpoelen@xs4all.nl>
2017-11-03 07:11:00 -0700

    removed notify scripts - freshdata does not send emails

--- notify.groovy
+++
@@ -1,5 +0,0 @@
-node {
-    stage 'notify subscribers'
-    dateString = sh([script: 'date --date \'2 weeks ago\' +%Y-%m-%d', returnStdout: true]).trim()
-    sh "curl --verbose \"http://api.effechecka.org/notifyAll?addedAfter=$dateString\""
-}
